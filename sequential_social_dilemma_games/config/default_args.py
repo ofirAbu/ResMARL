7a5c939b7f42d2118bf591dc1588a3d7237db4a3
ofir.abu <OfirAbu1>
2021-09-09 15:20:28 +0300

    changed default amount of GPUs to 0 for experimenting locally

--- sequential_social_dilemma_games/config/default_args.py
+++ sequential_social_dilemma_games/config/default_args.py
@@ -79,7 +79,7 @@ def add_default_args(parser):
         "--cpus_for_driver", type=int, default=0, help="Number of CPUs used by the driver"
     )
     parser.add_argument(
-        "--gpus_for_driver", type=float, default=1, help="Number of GPUs used by the driver"
+        "--gpus_for_driver", type=float, default=0, help="Number of GPUs used by the driver"
     )
     parser.add_argument(
         "--cpus_per_worker", type=int, default=1, help="Number of CPUs used by one worker"
