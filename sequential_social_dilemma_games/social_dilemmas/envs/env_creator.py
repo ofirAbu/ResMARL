35fb4e3be45e30e2f864efbbd3e243a01ec0e5f2
ofir.abu <OfirAbu1>
2022-01-01 22:46:28 +0200

    feat: refactored env_creator.py

--- sequential_social_dilemma_games/social_dilemmas/envs/env_creator.py
+++ sequential_social_dilemma_games/social_dilemmas/envs/env_creator.py
@@ -1,288 +1,65 @@
-from social_dilemmas.envs.cleanup import CleanupEnv, CleanupEnvWithMessagesSelf, CleanupEnvWithMessagesGlobal
+from social_dilemmas.envs.cleanup import CleanupEnv, CleanupEnvWithMessagesSelf, CleanupEnvWithMessagesGlobal, \
+    CleanupEnvWithMessagesMandatory
 from social_dilemmas.envs.envs_with_perturbations.cleanup_with_perts import CleanupPerturbationsEnv, \
-    CleanupPerturbationsEnvWithMessagesSelf, CleanupPerturbationsEnvWithMessagesGlobal
+    CleanupPerturbationsEnvWithMessagesSelf, CleanupPerturbationsEnvWithMessagesGlobal, \
+    CleanupPerturbationsEnvWithMessagesMandatory
 from social_dilemmas.envs.envs_with_perturbations.harvest_with_perts import HarvestPerturbationEnv, \
     HarvestPerturbationsEnvWithMessagesSelf, HarvestPerturbationsEnvWithMessagesGlobal, \
     HarvestPerturbationsEnvWithMessagesMandatory
-from social_dilemmas.envs.harvest import HarvestEnv, HarvestEnvWithMessagesSelf, HarvestEnvWithMessagesGlobal
+from social_dilemmas.envs.harvest import HarvestEnv, HarvestEnvWithMessagesSelf, HarvestEnvWithMessagesGlobal, \
+    HarvestEnvWithMessagesMandatory
 from social_dilemmas.envs.switch import SwitchEnv
 
 
 def get_env_creator(env, num_agents, args):
     # TODO add here envs with messages and confusion classes
-    if env == "harvest":
+    env_name_to_class = {
+        "harvest": HarvestEnv,
+        "harvest_msg_self": HarvestEnvWithMessagesSelf,
+        "harvest_msg_global": HarvestEnvWithMessagesGlobal,
+        "harvest_mandatory": HarvestEnvWithMessagesMandatory,
 
+        "cleanup": CleanupEnv,
+        "cleanup_msg_self": CleanupEnvWithMessagesSelf,
+        "cleanup_msg_global": CleanupEnvWithMessagesGlobal,
+        "cleanup_mandatory": CleanupEnvWithMessagesMandatory,
+    }
+    if env in list(env_name_to_class.keys()):
         def env_creator(_):
-            return HarvestEnv(
+            return env_name_to_class[env](
                 num_agents=num_agents,
                 return_agent_actions=True,
                 use_collective_reward=args.use_collective_reward,
             )
-
-    elif env == "harvest_msg_self":
+    else:
+        perturbation_magnitude = 70
+        if '150' in env:
+            perturbation_magnitude = 150
+        elif '200' in env:
+            perturbation_magnitude = 190
+        if 'cleanup' in env:
+            env_class = CleanupPerturbationsEnv
+            if 'msg_self' in env:
+                env_class = CleanupPerturbationsEnvWithMessagesSelf
+            elif 'msg_global' in env:
+                env_class = CleanupPerturbationsEnvWithMessagesGlobal
+            elif 'mandatory' in env:
+                env_class = CleanupPerturbationsEnvWithMessagesMandatory
+        elif 'harvest' in env:
+            env_class = HarvestPerturbationEnv
+            if 'msg_self' in env:
+                env_class = HarvestPerturbationsEnvWithMessagesSelf
+            elif 'msg_global' in env:
+                env_class = HarvestPerturbationsEnvWithMessagesGlobal
+            elif 'mandatory' in env:
+                env_class = HarvestPerturbationsEnvWithMessagesMandatory
 
         def env_creator(_):
-            return HarvestEnvWithMessagesSelf(
+            return env_class(
                 num_agents=num_agents,
                 return_agent_actions=True,
                 use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
+                perturbation_magnitude=perturbation_magnitude
             )
-
-    elif env == "harvest_msg_global":
-
-        def env_creator(_):
-            return HarvestEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-            )
-
-    elif env == "harvest_mandatory":
-
-        def env_creator(_):
-            return HarvestPerturbationsEnvWithMessagesMandatory(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-            )
-
-    elif env == "cleanup":
-
-        def env_creator(_):
-            return CleanupEnv(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-            )
-
-    elif env == "cleanup_msg_self":
-
-        def env_creator(_):
-            return CleanupEnvWithMessagesSelf(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-            )
-
-    elif env == "cleanup_msg_global":
-
-        def env_creator(_):
-            return CleanupEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-            )
-
-    # pert = 50
-    # cleanup
-    elif env == "cleanup_pert_50":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnv(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                perturbation_magnitude=70,
-            )
-
-    elif env == "cleanup_pert_50_msg_self":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnvWithMessagesSelf(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=70,
-            )
-
-    elif env == "cleanup_pert_50_msg_global":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=70,
-            )
-
-    # harvest
-    elif env == "harvest_pert_50":
-
-        def env_creator(_):
-            return HarvestPerturbationEnv(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                perturbation_magnitude=70,
-            )
-
-    elif env == "harvest_pert_50_msg_self":
-
-        def env_creator(_):
-            return HarvestPerturbationsEnvWithMessagesSelf(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=70,
-            )
-
-    elif env == "harvest_pert_50_msg_global":
-
-        def env_creator(_):
-            return HarvestPerturbationsEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=70,
-            )
-
-    # pert = 150
-    # cleanup
-    elif env == "cleanup_pert_150":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnv(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                perturbation_magnitude=150,
-            )
-
-    elif env == "cleanup_pert_150_msg_self":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnvWithMessagesSelf(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=150,
-            )
-
-    elif env == "cleanup_pert_150_msg_global":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=150,
-            )
-
-    # harvest
-    elif env == "harvest_pert_150":
-
-        def env_creator(_):
-            return HarvestPerturbationEnv(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                perturbation_magnitude=150,
-            )
-
-    elif env == "harvest_pert_150_msg_self":
-
-        def env_creator(_):
-            return HarvestPerturbationsEnvWithMessagesSelf(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=150,
-            )
-
-    elif env == "harvest_pert_150_msg_global":
-
-        def env_creator(_):
-            return HarvestPerturbationsEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=150,
-            )
-
-    # pert = 200
-    # cleanup
-    elif env == "cleanup_pert_200":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnv(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                perturbation_magnitude=180,
-            )
-
-    elif env == "cleanup_pert_200_msg_self":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnvWithMessagesSelf(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=180,
-            )
-
-    elif env == "cleanup_pert_200_msg_global":
-
-        def env_creator(_):
-            return CleanupPerturbationsEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=180,
-            )
-
-    # harvest
-    elif env == "harvest_pert_200":
-
-        def env_creator(_):
-            return HarvestPerturbationEnv(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                perturbation_magnitude=180,
-            )
-
-    elif env == "harvest_pert_200_msg_self":
-
-        def env_creator(_):
-            return HarvestPerturbationsEnvWithMessagesSelf(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=180,
-            )
-
-    elif env == "harvest_pert_200_msg_global":
-
-        def env_creator(_):
-            return HarvestPerturbationsEnvWithMessagesGlobal(
-                num_agents=num_agents,
-                return_agent_actions=True,
-                use_collective_reward=args.use_collective_reward,
-                use_messages_attribute=True,
-                perturbation_magnitude=180,
-            )
-
-
-    elif env == "switch":
-
-        def env_creator(_):
-            return SwitchEnv(num_agents=num_agents, args=args)
 
     return env_creator
